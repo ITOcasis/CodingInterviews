91d0c3ee47c77f4e328f1f6a9a398fbf97614f76
qingchen1998 <1085776129@qq.com>
2020-06-14 15:59:29 +0800

    【出题】第三天，牛客网，JZ39，把二叉树打印成多行

---
+++ src/algorithm/Day03JZ39Print/TreeNode.java
@@ -0,0 +1,40 @@
+package algorithm.Day03JZ39Print;
+
+public class TreeNode {
+    public int val = 0;
+    public TreeNode left = null;
+    public TreeNode right = null;
+
+    public TreeNode(int val) {
+        this.val = val;
+    }
+
+    /**
+     * @return 返回叶子节点的值
+     */
+    @Override
+    public String toString() {
+        return this.val + "";
+    }
+
+    /**
+     * 先序遍历二叉树
+     * 
+     * @param root 二叉树的根节点
+     */
+    public static void visitBiTree(TreeNode root) {
+        if (root == null) {
+            System.out.println("此树无节点！");
+            return;
+        }
+
+        System.out.print(root.toString()+" ");
+        if (root.left != null) {
+            visitBiTree(root.left);
+        }
+
+        if (root.right != null) {
+            visitBiTree(root.right);
+        }
+    }
+}
