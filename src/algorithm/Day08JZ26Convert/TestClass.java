b17bfe06fa74c594ea50bcc63b087172baba658c
qingchen1998 <1085776129@qq.com>
2020-06-19 17:24:41 +0800

    【解题】第八天，牛客网，JZ26，二叉搜索树与双向链表

--- src/algorithm/Day08JZ26Convert/TestClass.java
+++ src/algorithm/Day08JZ26Convert/TestClass.java
@@ -1,55 +1,75 @@
 package algorithm.Day08JZ26Convert;
 
-import algorithm.Day06JZ22PrintFromTopToBottom.qingchen.PrintFromTopToBottomQC;
+
+import algorithm.Day08JZ26Convert.qingchen.ConvertQC;
 
 public class TestClass {
-     /**
-     * 初始化一棵树:{1,2,3,4,5,#,6,#,#,7}
-     *              1
-      *          2     3
-      *        4   5     6
-      *          7
+    /**
+     * 初始化一棵树:
+     *                33
+     *        17              50
+     *    13     18       34      58
+     *      16      25        51      66
+     *                 27
+     *
      * @return
      */
     public static TreeNode init() {
-        TreeNode root = new TreeNode(1);
-
-        TreeNode node1 = new TreeNode(2);
-        TreeNode node2 = new TreeNode(3);
-
-        TreeNode node3 = new TreeNode(4);
-        TreeNode node4 = new TreeNode(5);
-
-        TreeNode node5 = new TreeNode(7);
-        TreeNode node6 = new TreeNode(6);
-
+        TreeNode root = new TreeNode(33);
+        TreeNode node1 = new TreeNode(17);
+        TreeNode node2 = new TreeNode(50);
         root.left = node1;
         root.right = node2;
-
-        node1.left = node3;
-        node1.right = node4;
-
-        node4.left = node5;
-
-        node2.right = node6;
+        TreeNode node3 = new TreeNode(13);
+        TreeNode node4 = new TreeNode(18);
+        node1.left=node3;
+        node1.right=node4;
+        TreeNode node5 = new TreeNode(16);
+        node3.right = node5;
+        TreeNode node6 = new TreeNode(25);
+        node4.right = node6;
+        TreeNode node7 = new TreeNode(27);
+        node6.right = node7;
+        TreeNode node8 = new TreeNode(34);
+        TreeNode node9 = new TreeNode(58);
+        node2.left = node8;
+        node2.right=node9;
+        TreeNode node10 = new TreeNode(51);
+        TreeNode node11 = new TreeNode(66);
+        node9.left = node10;
+        node9.right = node11;
         return root;
     }
 
-    public static TreeNode init2() {
-        TreeNode root = new TreeNode(1);
-        TreeNode node1 = new TreeNode(2);
-        TreeNode node2 = new TreeNode(2);
-        root.left = node1;
-        root.right = node2;
-        TreeNode node3 = new TreeNode(3);
-        TreeNode node4 = new TreeNode(3);
-        node1.right = node3;
-        node2.right = node3;
+    public static TreeNode init2(){
+        TreeNode root = new TreeNode(10);
+        TreeNode n1 = new TreeNode(7);
+        TreeNode n2 = new TreeNode(13);
+        root.left = n1;
+        root.right = n2;
+        TreeNode n3 = new TreeNode(4);
+        TreeNode n4 = new  TreeNode(8);
+        n1.left = n3;
+        n1.right = n4;
+
         return root;
     }
 
+    public static TreeNode init3(){
+        TreeNode root = new TreeNode(5);
+        TreeNode n1 = new TreeNode(4);
+        root.left = n1;
+        TreeNode n2 = new TreeNode(3);
+        n1.left = n2;
+        TreeNode n3 = new TreeNode(2);
+        n2.left = n3;
+        TreeNode n4 = new TreeNode(1);
+        n3.left = n4;
+        return root;
+    }
 
     public static void main(String[] args) {
-
+        TreeNode convert = ConvertQC.Convert2(init3());
+        System.out.println(convert);
     }
 }
--- src/algorithm/Day08JZ26Convert/qingchen/ConvertQC.java
+++ src/algorithm/Day08JZ26Convert/qingchen/ConvertQC.java
@@ -1,7 +1,71 @@
 package algorithm.Day08JZ26Convert.qingchen;
 
+import algorithm.Day08JZ26Convert.TreeNode;
+
+import java.util.Stack;
+
 /**
  * @author Created by ChenJiaQiang on 2020/6/19 10:14
+ * 输入一棵二叉搜索树，将该二叉搜索树转换成一个排序的双向链表。要求不能创建任何新的结点，只能调整树中结点指针的指向。
  */
 public class ConvertQC {
+    /**
+     * 我的思路：“不能创建任何新的结点”。采用中序遍历，弄几个变量，分别指向当前节点的上一个结点，在遍历的时候，就调整位置.
+     * 非递归的方式看着挺复杂，其实就是递归的模板。下面再写下递归版。
+     * @param root
+     * @return
+     */
+    public static TreeNode Convert(TreeNode root) {
+        if (root==null)return null;
+        Stack<TreeNode> stack = new Stack<>();
+        TreeNode last = null;
+        TreeNode cur = root;
+        while (cur!=null||!stack.isEmpty()){
+            while (cur!=null){
+                stack.push(cur);
+                cur = cur.left;
+            }
+            cur = stack.pop();
+            if (last!=null) {
+                // 上一个结点指向当前结点
+                last.right = cur;
+                // 当前结点指向上一个结点
+                cur.left = last;
+            }
+            last = cur;
+            System.out.print(cur.val+" ");
+            cur = cur.right;
+        }
+        // 返回第一个结点
+        cur = root;
+        while (cur.left!=null){
+            cur = cur.left;
+        }
+        return cur;
+    }
+
+    public static TreeNode lastNode = null;
+
+    public static TreeNode Convert2(TreeNode root) {
+        inOrderConvert(root);
+        // 返回第一个结点
+        while (root!=null && root.left!=null){
+            root = root.left;
+        }
+        return root;
+    }
+
+    public static void inOrderConvert(TreeNode node){
+        if (node==null)return;
+        inOrderConvert(node.left);
+        // 当前结点的上一个结点
+        node.left = lastNode;
+        if (lastNode!=null){
+            // 上一个结点的下一个结点
+            lastNode.right = node;
+        }
+        // 更新 lastNode
+        lastNode = node;
+        inOrderConvert(node.right);
+    }
 }
