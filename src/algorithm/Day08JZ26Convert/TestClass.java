dc949fc8e5ea03e441d26a26bb13a98e4dd670de
qingchen1998 <1085776129@qq.com>
2020-06-19 10:15:37 +0800

    【出题】第八天，牛客网，JZ26，二叉搜索树与双向链表

---
+++ src/algorithm/Day08JZ26Convert/TestClass.java
@@ -0,0 +1,55 @@
+package algorithm.Day08JZ26Convert;
+
+import algorithm.Day06JZ22PrintFromTopToBottom.qingchen.PrintFromTopToBottomQC;
+
+public class TestClass {
+     /**
+     * 初始化一棵树:{1,2,3,4,5,#,6,#,#,7}
+     *              1
+      *          2     3
+      *        4   5     6
+      *          7
+     * @return
+     */
+    public static TreeNode init() {
+        TreeNode root = new TreeNode(1);
+
+        TreeNode node1 = new TreeNode(2);
+        TreeNode node2 = new TreeNode(3);
+
+        TreeNode node3 = new TreeNode(4);
+        TreeNode node4 = new TreeNode(5);
+
+        TreeNode node5 = new TreeNode(7);
+        TreeNode node6 = new TreeNode(6);
+
+        root.left = node1;
+        root.right = node2;
+
+        node1.left = node3;
+        node1.right = node4;
+
+        node4.left = node5;
+
+        node2.right = node6;
+        return root;
+    }
+
+    public static TreeNode init2() {
+        TreeNode root = new TreeNode(1);
+        TreeNode node1 = new TreeNode(2);
+        TreeNode node2 = new TreeNode(2);
+        root.left = node1;
+        root.right = node2;
+        TreeNode node3 = new TreeNode(3);
+        TreeNode node4 = new TreeNode(3);
+        node1.right = node3;
+        node2.right = node3;
+        return root;
+    }
+
+
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/algorithm/Day08JZ26Convert/qingchen/ConvertQC.java
@@ -0,0 +1,7 @@
+package algorithm.Day08JZ26Convert.qingchen;
+
+/**
+ * @author Created by ChenJiaQiang on 2020/6/19 10:14
+ */
+public class ConvertQC {
+}
